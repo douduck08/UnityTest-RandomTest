c3b81ea41d07f3e7618242e6dea9a2e53c56f7dc
douduck08 <lu10082000@gmail.com>
2017-04-06 14:48:19 +0800

    test asset loading

---
+++ Assets/TestingUnit/TextAssetLoading/LoadingTextAsset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LoadingTextAsset : MonoBehaviour {
+
+    public string filePath;
+    private Text m_text;
+
+    void Start () {
+        m_text = GetComponent<Text> ();
+    }
+
+}
